51535d180ee1a151543e65f47e415260078cba60
Katsiaryna <yarovaya2631@gmail.com>
2025-11-10 13:00:01 +0300

    DB connection

---
+++ SiteEventsAggregator/php/db_connection.php
@@ -0,0 +1,10 @@
+<?php
+$mysqli=new mysqli('MySQL-8.4','root','','events_aggregator');
+if ($mysqli->connect_error) {
+    die("Ошибка подключения к БД: " . $mysqli->connect_error);
+    echo "error";
+}
+else{
+    echo " db connected";
+}
+?>
