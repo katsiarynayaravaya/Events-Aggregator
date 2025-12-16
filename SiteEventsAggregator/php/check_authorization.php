ee3bd67d660a1c23fd9c4cea9d718670d0165471
Katsiaryna <yarovaya2631@gmail.com>
2025-12-16 17:58:02 +0300

    favorites logic

--- SiteEventsAggregator/php/check_authorization.php
+++ SiteEventsAggregator/php/check_authorization.php
@@ -1,9 +1,26 @@
 <?php
 session_start();
 
+$response = [
+    'logged_in' => false
+];
+
 if (isset($_SESSION['user_id'])) {
-    echo '{"logged_in":true,"user":{"name":"' . $_SESSION['user_name'] . '"}}';
+    $response['logged_in'] = true;
+    $response['user_id'] = $_SESSION['user_id'];
+    $response['user_name'] = $_SESSION['user_name'] ?? '';
+}
+
+// Проверяем, запрашивают ли JSON
+if (isset($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false) {
+    header('Content-Type: application/json');
+    echo json_encode($response);
 } else {
-    echo '{"logged_in":false}';
+    // Старый формат для обратной совместимости
+    if ($response['logged_in']) {
+        echo '{"logged_in":true,"user":{"name":"' . $response['user_name'] . '"}}';
+    } else {
+        echo '{"logged_in":false}';
+    }
 }
 ?>
